6db73dd4406ae8f8724dd3ec79499ba7bdbf8327
Hasan Kayahan <112942290+hasankayahan@users.noreply.github.com>
2023-09-28 22:26:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,34 @@ import pandas as pd
 import requests
 from plotly import offline
 import plotly.express as px
+import lxml  # lxml kütüphanesini içe aktarın
 
 url = "https://www.isyatirim.com.tr/tr-tr/analiz/hisse/Sayfalar/Temel-Degerler-Ve-Oranlar.aspx#page-1"
-tablo = pd.read_html(requests.get(url).text)[2]
+
+# Tablo 1
+tablo = pd.read_html(requests.get(url).text, flavor="lxml")[2]
 sektör = pd.DataFrame({
     "Hisse": tablo["Kod"],
     "Sektör": tablo["Sektör"],
     "Piyasa Değeri (mn $)": tablo["Piyasa Değeri (mn $)"].str.replace(',', '.').str.replace('.', '', 1).astype(float)
 })
 
-tablo2 = pd.read_html(requests.get(url).text)[7]
+# Tablo 2
+tablo2 = pd.read_html(requests.get(url).text, flavor="lxml")[7]
 tablo2["Günlük Getiri (%)"] = tablo2["Günlük Getiri (%)"].replace('-', '0').str.replace(',', '.').astype(float) / 100
-
 getiri = pd.DataFrame({
     "Hisse": tablo2["Kod"],
     "Getiri (%)": tablo2["Günlük Getiri (%)"]
 })
 
+# Verileri Birleştir
 df = pd.merge(sektör, getiri, on="Hisse")
 
+# Renkler
 renk_aralık = [-10, -5, -0.01, 0, 0.01, 5, 10]
 df["Renk"] = pd.cut(df["Getiri (%)"], bins=renk_aralık, labels=["red", "indianred", "lightpink", "lightgreen", "lime", "green"])
 
+# Plotly Grafiği
 fig = px.treemap(df, path=[px.Constant("Borsa İstanbul"), "Sektör", "Hisse"],
                  values="Piyasa Değeri (mn $)", color="Renk",
                  custom_data=["Getiri (%)", "Sektör"],
@@ -42,4 +48,5 @@ fig.update_traces(
 
 fig.data[0].texttemplate = "<b>%{label}</b><br>%{customdata[0]} %"
 
+# Grafiği Kaydet
 offline.plot(fig, filename="C:/Users/hasan/OneDrive/Desktop/grafik.html")
